650f5dcdb487848623472fe513c67a5c4a557312
Tam Tran <tam@broadbean.com>
2020-06-28 17:38:41 -0700

    Fixed route

--- src/App/Main.js
+++ src/App/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import CustomerTable from './Customer';
 import FilmTable from './Film';
 
-const APIBaseUrl = window.location.protocol + "//" + window.location.hostname + ":5004"
+const APIBaseUrl = window.location.protocol + "//" + window.location.hostname + ":50041"
 const CustomerAPI = APIBaseUrl + "/api/datatable/customers";
 const FilmAPI = APIBaseUrl + "/api/datatable/films";
+
+const redirectURL = window.location.protocol + "//" + window.location.hostname
 
 const Main = () => (
   <Switch>
@@ -29,7 +31,7 @@ const Main = () => (
         }
       }>
     </Route>
-    <Redirect to='/home' />
+    <Route exact path="/" render={() => (window.location = redirectURL)} />
   </Switch>
 );
 
